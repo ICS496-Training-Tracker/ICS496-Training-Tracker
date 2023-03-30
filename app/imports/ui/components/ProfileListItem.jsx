2729bdd5d700751bc6999a13cf4201c3c0b125da
tkansaki <tkansaki@hawaii.edu>
2023-03-30 12:34:20 -1000

    Create ProfileListItem.jsx

---
+++ app/imports/ui/components/ProfileListItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+
+const ProfileListItem = ({ profile }) => {
+
+  const viewHandler = () => {
+    console.log(`view ${profile.userID}`);
+  };
+
+  return (
+    <tr>
+      <td>{profile.lastName}</td>
+      <td>{profile.firstName}</td>
+      <td>{profile.rank}</td>
+      <td>{profile.afsc}</td>
+      <td>{profile.status}</td>
+      <td><Button onClick={viewHandler}>View</Button></td>
+    </tr>
+  );
+};
+
+ProfileListItem.propTypes = {
+  profile: PropTypes.shape({
+    email: PropTypes.string,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    status: PropTypes.number,
+    afsc: PropTypes.string,
+    rank: PropTypes.string,
+    unit: PropTypes.string,
+    role: PropTypes.string,
+    userID: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProfileListItem;
